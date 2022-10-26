35fd7eccea311d2cd894dbc360dc14af425a60d2
StromTina <112858034+StromTina@users.noreply.github.com>
2022-10-26 09:45:02 +0200

    added scoring and out of bounce for players and balls

--- js/game.js
+++ js/game.js
@@ -12,6 +12,8 @@ class Game {
         this.context = context;
         this.deltaTime = 0;
         this.spawnTimer = 0;
+        this.player1Points = 0;
+        this.player2Points = 0;
         this.player1 = new Player(new Position(this.canvas.width * 0.25, this.canvas.height - 20));
         this.player2 = new Player(new Position(this.canvas.width * 0.75, this.canvas.height - 20));
         this.balls = [];
@@ -20,6 +22,18 @@ class Game {
     start() {
         tick();
     }
+
+    drawPoints() {
+        this.context.fillStyle = 'white';
+        this.context.font = '48px serif';
+        this.context.textAlign = 'center';
+        this.context.fillText(this.player1Points, (this.canvas.width / 2) - 100, this.canvas.height - 10);
+
+        this.context.fillStyle = 'white';
+        this.context.font = '48px serif';
+        this.context.textAlign = 'center';
+        this.context.fillText(this.player2Points, (this.canvas.width / 2) + 100, this.canvas.height - 10);
+    }
 }
 
 export const game = new Game(canvas, context);
@@ -27,6 +41,7 @@ export const game = new Game(canvas, context);
 let tickCount = 0;
 let lastTime = Date.now();
 let spawnTimer = 0;
+
 function tick() {
     let currentTime = Date.now();
     game.deltaTime = (currentTime - lastTime) / 1000;
@@ -39,6 +54,8 @@ function tick() {
     game.context.fillStyle = 'black';
     game.context.fillRect(0, 0, game.canvas.width, game.canvas.height);
 
+    game.drawPoints();
+
     game.player1.tick(game);
     game.player2.tick(game);
     game.player1.draw(game);
@@ -48,6 +65,11 @@ function tick() {
         let ball = game.balls[i];
         ball.draw(game);
         ball.tick(game);
+
+        if (ball.position.x < 0 || ball.position.x > game.canvas.width) {   //added this to avoid lagging - somehow makes the balls blink(?), maybe fix later?
+            game.balls.splice(0, 1);
+        }
+
     }
 
     requestAnimationFrame(tick);
@@ -55,16 +77,16 @@ function tick() {
 
 //tick();
 
-function spawnBall(inGame){
+function spawnBall(inGame) {
     inGame.spawnTimer += inGame.deltaTime;
-    if(inGame.spawnTimer > 0.2){
+    if (inGame.spawnTimer > 0.2) {
         inGame.spawnTimer = 0;
         console.log("spawnTime: " + inGame.spawnTimer);
-        if(trueOrFalse()){  //maybe make this one nicer
-            if(trueOrFalse()){
+        if (trueOrFalse()) {  //maybe make this one nicer
+            if (trueOrFalse()) {
                 inGame.balls.push(new BallFromLeft(new Position(0, Math.floor(Math.random() * (inGame.canvas.height - 100)))));
             }
-            else{
+            else {
                 inGame.balls.push(new BallFromRight(new Position(inGame.canvas.width, Math.floor(Math.random() * (inGame.canvas.height - 100)))))
             }
         }
--- js/player.js
+++ js/player.js
@@ -1,10 +1,11 @@
-import { Entity } from './entity.js';
+import { Position, Entity } from './entity.js';
+//import { game } from './game.js';
 
 export class Player extends Entity {
     constructor(position) {
         super(position);
         this.width = 20;
-        this.height = 35;
+        this.height = 20;
         this.up = false;
         this.down = false;
     }
@@ -15,16 +16,28 @@ export class Player extends Entity {
         } else if (this.down) {
             this.position.y += 100 * game.deltaTime;
         }
+
+        if (this.position.y > game.canvas.height - (this.height / 2)) {
+            this.position.y = game.canvas.height - (this.height / 2);
+        }
+
+        if (this.position.x < game.canvas.width / 2 && this.position.y < 0) {
+            game.player1 = new Player(new Position(game.canvas.width * 0.25, game.canvas.height - 20));
+            game.player1Points++
+        } else if (this.position.x > game.canvas.width / 2 && this.position.y < 0) {
+            game.player2 = new Player(new Position(game.canvas.width * 0.75, game.canvas.height - 20));
+            game.player2Points++
+        }
     }
 
     draw(game) {
-        //game.context.fillStyle = 'black';
-        //game.context.fillRect(this.position.x + 15, this.position.y - 32, this.width, this.height);
+       // game.context.fillStyle = 'white';
+       // game.context.fillRect(this.position.x, this.position.y, this.width, this.height);
         game.context.save();
         game.context.translate(this.position.x, this.position.y);
-        game.context.fill();
-        game.context.font = '40px serif';
-        game.context.rotate(-Math.PI/4);
+        //game.context.fill();
+        game.context.font = '45px serif';
+        game.context.rotate(-Math.PI / 4);
         game.context.fillText('🚀', 0, 0);
         game.context.restore();
     }
